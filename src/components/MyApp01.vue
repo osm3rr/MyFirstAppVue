31279b5ebaade674d75f9ba8a046f9cdb1800633
osm3rr <osm3rr@mail.com>
2024-12-14 15:24:20 -0400

    first commit

---
+++ src/components/MyApp01.vue
@@ -0,0 +1,18 @@
+
+<script>
+</script>
+
+<template>
+    <div>Mi primera app en <span>Vue.Js</span></div>
+</template>
+
+<style scoped>
+    div {
+        color: #35495f;
+        font-size: 1.6rem;
+        }
+    span {
+        color: #41b883;
+        font-weight: 700;
+    }
+</style>
